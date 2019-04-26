33009f87bf7b131b19c15a72c26179724ac1bf08
Trent Moss <trentmoss05@gmail.com>
2019-04-26 17:42:05 +0000

    Automatically backed up by Learn

--- js/selectors.js
+++ js/selectors.js
@@ -6,3 +6,15 @@ function paragraphSelector() {
 function lastImageSelector() {
   $('img:last')
 }
+
+function ninjaBabySelector() {
+  $('#baby-ninja')
+}
+
+function divSelector() {
+  $('.pics')
+}
+
+function firstListItem() {
+  $('ul li:first-child #pic-list')
+}
